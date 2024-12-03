ff678dc136207d08b33f3e38c78512defa7e0064
janositibor <janositibor@gmail.com>
2024-12-03 13:01:08 +0100

    2024 day3 part2 solved

--- src/main/java/TZJanosi/y2024/day03/MulParametersExtractor.java
+++ src/main/java/TZJanosi/y2024/day03/MulParametersExtractor.java
@@ -1,7 +1,6 @@
 package TZJanosi.y2024.day03;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,11 +8,18 @@ import java.util.regex.Pattern;
 public class MulParametersExtractor {
     private String inputString;
     private List<List<Integer>> mulParameters=new ArrayList<>();
-    private long resultOfMultiplication;
+    private long resultOfMultiplication=0;
 
     public MulParametersExtractor(String inputString) {
         this.inputString = inputString;
-        extractParametersFromInputString();
+    }
+
+    public void calculateResultForAllParameters(){
+        extractParametersFromString(inputString);
+        calculateResult();
+    }
+    public void calculateResultForEnabledParameters(){
+        extractEnabledStrings(inputString);
         calculateResult();
     }
 
@@ -22,25 +28,39 @@ public class MulParametersExtractor {
         for (List<Integer> parameters:mulParameters) {
             result+=(parameters.get(0)*parameters.get(1));
         }
-        resultOfMultiplication=result;
+        resultOfMultiplication+=result;
     }
 
-    private void extractParametersFromInputString(){
+
+    private void extractEnabledStrings(String input){
+        String stop="don't()";
+        String start="do()";
+        if(input.indexOf(start)==0){
+            input=input.substring(start.length());
+        }
+        int stopLocation=input.indexOf(stop);
+        stopLocation=(stopLocation>0?stopLocation:input.length());
+        String enabled=input.substring(0,stopLocation);
+        extractParametersFromString(enabled);
+        int startLocation=input.indexOf(start,stopLocation);
+        if(startLocation>0){
+            String needMoreProcessing=input.substring(startLocation);
+            extractEnabledStrings(needMoreProcessing);
+        }
+    }
+
+    private void extractParametersFromString(String input){
         Pattern MY_PATTERN = Pattern.compile("mul\\([0-9]{1,3}+,[0-9]{1,3}+\\)");
-        Matcher m = MY_PATTERN.matcher(inputString);
+        Matcher m = MY_PATTERN.matcher(input);
         while (m.find()) {
             String s = m.group(0);
-//            System.out.println(s);
             mulParameters.add(extractNumbersFromString(s));
         }
     }
     private List<Integer> extractNumbersFromString(String input){
         int locationOfComa=input.indexOf(',');
-//        System.out.println(input.substring(4,locationOfComa));
-//        System.out.println(input.substring(locationOfComa+1,input.length()-1));
         int number1=Integer.parseInt(input.substring(4,locationOfComa));
         int number2=Integer.parseInt(input.substring(locationOfComa+1,input.length()-1));
-//        System.out.println(number1 + " * " +number2);
         return List.of(number1,number2);
     }
 

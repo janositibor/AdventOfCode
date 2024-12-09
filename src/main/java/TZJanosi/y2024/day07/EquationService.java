0a533fe37aee2bde986e78d5b71043aaa16ab17a
janositibor <janositibor@gmail.com>
2024-12-09 03:32:31 +0100

    2024 day7 part1 solved

---
+++ src/main/java/TZJanosi/y2024/day07/EquationService.java
@@ -0,0 +1,29 @@
+package TZJanosi.y2024.day07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EquationService {
+    private List<Long> expectedResults;
+    private List<List<Integer>> operands;
+
+    public EquationService(List<Long> expectedResults, List<List<Integer>> operands) {
+        if(expectedResults.size()!= operands.size()){
+            throw new IllegalArgumentException("Different List size!\n"+expectedResults.size() + " vs. " + operands.size());
+        }
+        this.expectedResults = expectedResults;
+        this.operands = operands;
+    }
+
+    public long calculate(){
+        long result=0;
+        for (int i = 0; i < expectedResults.size(); i++) {
+            Equation equation=new Equation(expectedResults.get(i),operands.get(i));
+            if(equation.isValid()){
+                result+=expectedResults.get(i);
+            }
+        }
+        return result;
+    }
+
+}

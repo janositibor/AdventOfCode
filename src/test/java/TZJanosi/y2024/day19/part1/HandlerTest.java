4e4fcf780335ec41eea76c3a89b5fe20fdfe9122
janositibor <janositibor@gmail.com>
2025-04-28 11:07:06 +0200

    2024 day19 part1 solved

---
+++ src/test/java/TZJanosi/y2024/day19/part1/HandlerTest.java
@@ -0,0 +1,54 @@
+package TZJanosi.y2024.day19.part1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandlerTest {
+    @Test
+    void getWords() {
+        ReadData readData = new ReadData("testInput.txt");
+        Handler handler = new Handler(readData.getOutput());
+        assertThat(handler.getWords())
+                .hasSize(8)
+                .containsExactly("brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb");
+    }
+
+    @Test
+    void getBricks() {
+        ReadData readData = new ReadData("testInput.txt");
+        Handler handler = new Handler(readData.getOutput());
+        assertThat(handler.getBricks())
+                .hasSize(8)
+                .containsOnly("r", "wr", "b", "g", "bwu", "rb", "gb", "br");
+        assertThat(handler.getBricks().get(0))
+                .isEqualTo("bwu");
+    }
+
+    @Test
+    void processData() {
+        ReadData readData = new ReadData("testInput.txt");
+        Handler handler = new Handler(readData.getOutput());
+        assertThat(handler.processData())
+                .isEqualTo(6);
+
+    }
+
+    @Test
+    void processProblemData() {
+        ReadData readData = new ReadData("input.txt");
+        Handler handler = new Handler(readData.getOutput());
+        assertThat(handler.processData())
+                .isEqualTo(324);
+
+    }
+
+    @Test
+    void difficultExampleProcessData() {
+        ReadData readData = new ReadData("input00.txt");
+        Handler handler = new Handler(readData.getOutput());
+        assertThat(handler.processData())
+                .isEqualTo(0);
+    }
+}

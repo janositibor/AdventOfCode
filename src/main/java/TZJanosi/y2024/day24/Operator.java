bc34aad8240bdbeb3266735cdb58121b1d6cd3ae
janositibor <janositibor@gmail.com>
2025-05-15 12:59:19 +0200

    2024 day24 part1 solved

---
+++ src/main/java/TZJanosi/y2024/day24/Operator.java
@@ -0,0 +1,24 @@
+package TZJanosi.y2024.day24;
+
+public enum Operator {
+    AND {
+        @Override
+        public boolean calculate(boolean o1, boolean o2) {
+            return o1 && o2;
+        }
+    },
+    OR {
+        @Override
+        public boolean calculate(boolean o1, boolean o2) {
+            return o1 || o2;
+        }
+    },
+    XOR {
+        @Override
+        public boolean calculate(boolean o1, boolean o2) {
+            return o1 ^ o2;
+        }
+    };
+
+    public abstract boolean calculate(boolean o1, boolean o2);
+}

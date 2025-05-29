4f3597a776eb42368d4b7a1cda33999afb124616
janositibor <janositibor@gmail.com>
2025-05-29 11:55:14 +0200

    2024 day25 part1 solved

---
+++ src/main/java/TZJanosi/y2024/day25/part1/Key.java
@@ -0,0 +1,29 @@
+package TZJanosi.y2024.day25.part1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Key {
+    private int id;
+    private List<Integer> heights = new ArrayList<>();
+
+    public Key(List<Integer> heights) {
+        this.heights = heights;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Key{" +
+                "id=" + id +
+                ", heights=" + heights +
+                '}';
+    }
+
+    public List<Integer> getHeights() {
+        return heights;
+    }
+}
---
+++ src/main/java/TZJanosi/y2024/day25/part1/Lock.java
@@ -0,0 +1,40 @@
+package TZJanosi.y2024.day25.part1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lock {
+    private int id;
+    private List<Integer> heights = new ArrayList<>();
+
+    public Lock(List<Integer> heights) {
+        this.heights = heights;
+    }
+
+
+    public boolean compatible(Key key) {
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights.get(i) + key.getHeights().get(i) > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Lock{" +
+                "id=" + id +
+                ", heights=" + heights +
+                '}';
+    }
+
+    public List<Integer> getHeights() {
+        return heights;
+    }
+}

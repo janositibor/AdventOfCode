6d890ff142219cc351fb8c6ab7c8a0ed1e6766e8
janositibor <janositibor@gmail.com>
2025-06-11 09:20:46 +0200

    2015 day13 part1 solved

---
+++ src/main/java/TZJanosi/y2015/day13/Handler.java
@@ -0,0 +1,36 @@
+package TZJanosi.y2015.day13;
+
+import java.util.List;
+
+public class Handler {
+    private List<String> input;
+    private int maximalHappiness = Integer.MIN_VALUE;
+
+    public Handler(List<String> input) {
+        this.input = input;
+    }
+
+    public int findHappiestWay() {
+        TablePlanner start = new TablePlanner(input);
+        buildHappiestWay(start);
+        return maximalHappiness;
+    }
+
+    private void buildHappiestWay(TablePlanner actual) {
+        int actualHappiness = actual.getHappiness();
+
+        List<String> lookForPlaces = actual.getLookForPlace();
+        if (lookForPlaces.isEmpty()) {
+            if (actualHappiness > maximalHappiness) {
+                maximalHappiness = actualHappiness;
+            }
+            return;
+        }
+        for (int i = 0; i < lookForPlaces.size(); i++) {
+            TablePlanner next = new TablePlanner(actual);
+            String name = lookForPlaces.get(i);
+            next.sit(name);
+            buildHappiestWay(next);
+        }
+    }
+}

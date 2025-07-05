835f41ba2d10155bb677efd37147e4d92060873e
janositibor <janositibor@gmail.com>
2025-07-05 23:15:52 +0200

    2016 day01 part1 solved

---
+++ src/main/java/TZJanosi/y2016/day01/Direction.java
@@ -0,0 +1,61 @@
+package TZJanosi.y2016.day01;
+
+public enum Direction {
+    NORTH(new Coordinate(0, 1)) {
+        @Override
+        public Direction turnClockWise() {
+            return EAST;
+        }
+
+        @Override
+        public Direction turnCounterClockWise() {
+            return WEST;
+        }
+    },
+    SOUTH(new Coordinate(0, -1)) {
+        @Override
+        public Direction turnClockWise() {
+            return WEST;
+        }
+
+        @Override
+        public Direction turnCounterClockWise() {
+            return EAST;
+        }
+    },
+    EAST(new Coordinate(1, 0)) {
+        @Override
+        public Direction turnClockWise() {
+            return SOUTH;
+        }
+
+        @Override
+        public Direction turnCounterClockWise() {
+            return NORTH;
+        }
+    },
+    WEST(new Coordinate(-1, 0)) {
+        @Override
+        public Direction turnClockWise() {
+            return NORTH;
+        }
+
+        @Override
+        public Direction turnCounterClockWise() {
+            return SOUTH;
+        }
+    };
+    private Coordinate direction;
+
+    Direction(Coordinate direction) {
+        this.direction = direction;
+    }
+
+    public abstract Direction turnClockWise();
+
+    public abstract Direction turnCounterClockWise();
+
+    public Coordinate getDirection() {
+        return direction;
+    }
+}

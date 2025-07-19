717241d442c02572f7d3a80e874d2540ee248e06
janositibor <janositibor@gmail.com>
2025-07-19 09:25:37 +0200

    2016 day03 part2 solved

--- src/main/java/tzjanosi/y2016/day03/Service.java
+++ src/main/java/tzjanosi/y2016/day03/Service.java
@@ -6,11 +6,36 @@ import java.util.List;
 public class Service {
     private List<Triangle> triangles = new ArrayList<>();
 
-    public Service(List<String> input) {
-        for (int i = 0; i < input.size(); i++) {
-            String line = input.get(i);
-            processLine(line);
+    public Service(List<String> input, boolean part2) {
+        if (part2) {
+            for (int i = 0; i < input.size(); i += 3) {
+                String line1 = input.get(i);
+                String line2 = input.get(i + 1);
+                String line3 = input.get(i + 2);
+                processLinePart2(line1, line2, line3);
+            }
+        } else {
+            for (int i = 0; i < input.size(); i++) {
+                String line = input.get(i);
+                processLine(line);
+            }
         }
+
+    }
+
+    private void processLinePart2(String line1, String line2, String line3) {
+        Triangle triangle;
+        for (int i = 0; i <= 10; i += 5) {
+            triangle = createTriangle(i, line1, line2, line3);
+            triangles.add(triangle);
+        }
+    }
+
+    private Triangle createTriangle(int index, String line1, String line2, String line3) {
+        int a = Integer.parseInt(line1.substring(index, index + 5).strip());
+        int b = Integer.parseInt(line2.substring(index, index + 5).strip());
+        int c = Integer.parseInt(line3.substring(index, index + 5).strip());
+        return new Triangle(a, b, c);
     }
 
     private void processLine(String line) {
--- src/test/java/tzjanosi/y2016/day03/ServiceTest.java
+++ src/test/java/tzjanosi/y2016/day03/ServiceTest.java
@@ -8,9 +8,17 @@ class ServiceTest {
     @Test
     void serviceTest() {
         ReadData readData = new ReadData("input.txt");
-        Service service = new Service(readData.getOutput());
+        Service service = new Service(readData.getOutput(), false);
 
         assertEquals(982, service.countValidTriangles());
+    }
+
+    @Test
+    void servicePart2Test() {
+        ReadData readData = new ReadData("input.txt");
+        Service service = new Service(readData.getOutput(), true);
+
+        assertEquals(1826, service.countValidTriangles());
     }
 
 }

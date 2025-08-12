5eb9897cbedcfff54bf3d0be5cf4e65b35436f31
janositibor <janositibor@gmail.com>
2025-08-13 00:41:02 +0200

    2016 day19 part2 solved

--- src/main/java/tzjanosi/y2016/day19/Game.java
+++ src/main/java/tzjanosi/y2016/day19/Game.java
@@ -11,7 +11,40 @@ public class Game {
         }
     }
 
-    public int findTheWinner() {
+    public int findTheWinnerPart2() {
+        int thief = 1;
+        while (players.size() > 1) {
+            thief = roundPart2(thief);
+        }
+        return players.get(0);
+    }
+
+    private int roundPart2(int fromValue) {
+        Set<Integer> playersToRemove = new HashSet<>();
+        int actualNumberOfPlayers = players.size();
+        int shift = actualNumberOfPlayers / 2;
+        int from = players.indexOf(fromValue);
+
+        for (int i = from; i < from + shift; i += 3) {
+            int indexToRemove = (i + shift);
+            playersToRemove.add(players.get((indexToRemove) % actualNumberOfPlayers));
+            if (actualNumberOfPlayers % 2 == 0 && actualNumberOfPlayers > 2) {
+                playersToRemove.add(players.get((indexToRemove + 1) % actualNumberOfPlayers));
+            }
+            if (actualNumberOfPlayers % 2 == 1 && actualNumberOfPlayers > 3) {
+                playersToRemove.add(players.get((indexToRemove + 2) % actualNumberOfPlayers));
+            }
+        }
+        int counter = from + playersToRemove.size();
+        while (playersToRemove.contains(players.get((counter) % actualNumberOfPlayers))) {
+            counter++;
+        }
+        int output = players.get((counter) % actualNumberOfPlayers);
+        players.removeAll(playersToRemove);
+        return output;
+    }
+
+    public int findTheWinnerPart1() {
         while (players.size() > 1) {
             round();
         }
--- src/test/java/tzjanosi/y2016/day19/GameTest.java
+++ src/test/java/tzjanosi/y2016/day19/GameTest.java
@@ -6,14 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     @Test
-    void findTheWinnerTest() {
+    void findTheWinnerPart1Test() {
         Game game = new Game(5);
-        assertEquals(3, game.findTheWinner());
+        assertEquals(3, game.findTheWinnerPart1());
     }
 
     @Test
-    void findTheWinnerProgramDataTest() {
+    void findTheWinnerProgramDataPart1Test() {
         Game game = new Game(3014387);
-        assertEquals(1834471, game.findTheWinner());
+        assertEquals(1834471, game.findTheWinnerPart1());
+    }
+
+    @Test
+    void findTheWinnerPart2Test() {
+        Game game = new Game(5);
+        assertEquals(2, game.findTheWinnerPart2());
+    }
+
+    @Test
+    void findTheWinnerPart2Test2() {
+        Game game = new Game(10);
+        assertEquals(1, game.findTheWinnerPart2());
+        assertEquals(1, game.findTheWinnerPart2());
+    }
+
+    @Test
+    void findTheWinnerPart2Test3() {
+        Game game = new Game(11);
+        assertEquals(2, game.findTheWinnerPart2());
+    }
+
+    @Test
+    void findTheWinnerProgramDataPart2Test() {
+        Game game = new Game(3014387);
+        assertEquals(1420064, game.findTheWinnerPart2());
     }
 }

a145efaab42e28a332a9c451f2b3cfc5d6810673
janositibor <janositibor@gmail.com>
2025-12-01 11:47:09 +0100

    2025 day01 part2 solved

--- src/main/java/tzjanosi/y2025/day01/Dial.java
+++ src/main/java/tzjanosi/y2025/day01/Dial.java
@@ -23,6 +23,25 @@ public class Dial {
         return output;
     }
 
+    public int calculatePasswordPart2() {
+        int output = 0;
+        int position = startingPosition;
+        for (int i = 0; i < steps.size(); i++) {
+            int step = steps.get(i);
+            for (int j = 0; j < Math.abs(step); j++) {
+                if (step < 0) {
+                    position--;
+                } else {
+                    position++;
+                }
+                if (position % 100 == 0) {
+                    output++;
+                }
+            }
+        }
+        return output;
+    }
+
     private void processInput(List<String> input) {
         for (int i = 0; i < input.size(); i++) {
             Integer step = createStepFromLine(input.get(i));
--- src/test/java/tzjanosi/y2025/day01/DialTest.java
+++ src/test/java/tzjanosi/y2025/day01/DialTest.java
@@ -19,4 +19,20 @@ class DialTest {
         assertEquals(1081, dial.calculatePassword());
     }
 
+    @Test
+    void calculatePasswordPart2Test() {
+        ReadData readData = new ReadData("testInput.txt");
+        Dial dial = new Dial(readData.getOutput());
+        assertEquals(6, dial.calculatePasswordPart2());
+    }
+
+    @Test
+    void calculatePasswordPart2ProgramDataTest() {
+        ReadData readData = new ReadData("input.txt");
+        Dial dial = new Dial(readData.getOutput());
+        assertEquals(6689, dial.calculatePasswordPart2());
+    }
+
+
+
 }

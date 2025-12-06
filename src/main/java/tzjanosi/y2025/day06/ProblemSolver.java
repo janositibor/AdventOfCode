181aced9255daea43c1af68db36ffb2a6091c462
janositibor <janositibor@gmail.com>
2025-12-06 09:00:26 +0100

    2025 day06 part2 solved

--- src/main/java/tzjanosi/y2025/day06/ProblemSolver.java
+++ src/main/java/tzjanosi/y2025/day06/ProblemSolver.java
@@ -1,15 +1,105 @@
 package tzjanosi.y2025.day06;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProblemSolver {
     private List<Long> adds = new ArrayList<>();
     private List<Long> multiplications = new ArrayList<>();
     private List<Long> result = new ArrayList<>();
+    private List<Long> part2Numbers;
+    private List<Long> part2Result = new ArrayList<>();
+    private char[][] inputMatrix;
 
     public ProblemSolver(List<String> input) {
         processInput(input);
+        processInputForPart2(input);
+    }
+
+    public long calculateGrandTotalForPart2() {
+        buildPart2Result();
+        return part2Result.stream().mapToLong(Long::longValue).sum();
+    }
+
+    private void buildPart2Result() {
+        part2Numbers = new ArrayList<>();
+        for (int i = inputMatrix.length - 1; i >= 0; i--) {
+            i -= processMatrixRow(inputMatrix[i]);
+        }
+    }
+
+    private int processMatrixRow(char[] input) {
+        char[] numberAsArray = Arrays.copyOf(input, input.length - 1);
+        long value = Long.parseLong(new String(numberAsArray).trim());
+        part2Numbers.add(value);
+        char lastChar = input[input.length - 1];
+        if (lastChar == '+' || lastChar == '*') {
+            System.out.println(lastChar);
+            long result;
+            if (lastChar == '+') {
+                result = addPart2Numbers();
+            } else {
+                result = multiplyPart2Numbers();
+            }
+            part2Result.add(result);
+            part2Numbers = new ArrayList<>();
+            return 1;
+        }
+        return 0;
+    }
+
+    private long multiplyPart2Numbers() {
+        return part2Numbers.stream().reduce(1L, (d1, d2) -> d1 * d2);
+    }
+
+    private long addPart2Numbers() {
+        return part2Numbers.stream().reduce(0L, (d1, d2) -> d1 + d2);
+    }
+
+    private void processInputForPart2(List<String> input) {
+        initInputMatrix(input);
+        for (int i = 0; i < input.size(); i++) {
+            addLineToInputMatrix(i, input.get(i));
+        }
+        inputMatrix = transpose(inputMatrix);
+    }
+
+    private void initInputMatrix(List<String> input) {
+        int y = input.size();
+        int x = input.stream().mapToInt(String::length).max().getAsInt();
+        inputMatrix = new char[y][x];
+        for (char[] row : inputMatrix) {
+            Arrays.fill(row, ' ');
+        }
+    }
+
+    private void addLineToInputMatrix(int y, String line) {
+        char[] lineAsCharArray = line.toCharArray();
+        if (lineAsCharArray.length == inputMatrix[y].length) {
+            inputMatrix[y] = lineAsCharArray;
+        } else {
+            copyValuesIntoInputMatrix(y, lineAsCharArray);
+        }
+    }
+
+    private void copyValuesIntoInputMatrix(int y, char[] lineAsCharArray) {
+        for (int i = 0; i < lineAsCharArray.length; i++) {
+            inputMatrix[y][i] = lineAsCharArray[i];
+        }
+    }
+
+    private char[][] transpose(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        char[][] transposed = new char[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
     }
 
     public long calculateGrandTotal() {
@@ -53,5 +143,9 @@ public class ProblemSolver {
             adds.add(value);
             multiplications.add(value);
         }
+    }
+
+    public char[][] getInputMatrix() {
+        return inputMatrix;
     }
 }
--- src/test/java/tzjanosi/y2025/day06/ProblemSolverTest.java
+++ src/test/java/tzjanosi/y2025/day06/ProblemSolverTest.java
@@ -19,4 +19,42 @@ class ProblemSolverTest {
         assertEquals(5361735137219L, problemSolver.calculateGrandTotal());
     }
 
+    @Test
+    void initTest() {
+        ReadData readData = new ReadData("testInput.txt");
+        ProblemSolver problemSolver = new ProblemSolver(readData.getOutput());
+        assertArrayEquals(new char[]{'1', ' ', ' ', '*'}, problemSolver.getInputMatrix()[0]);
+        assertArrayEquals(new char[]{' ', ' ', '4', ' '}, problemSolver.getInputMatrix()[14]);
+        assertArrayEquals(new char[][]{
+                {'1', ' ', ' ', '*'},
+                {'2', '4', ' ', ' '},
+                {'3', '5', '6', ' '},
+                {' ', ' ', ' ', ' '},
+                {'3', '6', '9', '+'},
+                {'2', '4', '8', ' '},
+                {'8', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+                {' ', '3', '2', '*'},
+                {'5', '8', '1', ' '},
+                {'1', '7', '5', ' '},
+                {' ', ' ', ' ', ' '},
+                {'6', '2', '3', '+'},
+                {'4', '3', '1', ' '},
+                {' ', ' ', '4', ' '}}, problemSolver.getInputMatrix());
+
+    }
+
+    @Test
+    void calculateGrandTotalForPart2Test() {
+        ReadData readData = new ReadData("testInput.txt");
+        ProblemSolver problemSolver = new ProblemSolver(readData.getOutput());
+        assertEquals(3263827L, problemSolver.calculateGrandTotalForPart2());
+    }
+
+    @Test
+    void calculateGrandTotalForPart2ProblemDataTest() {
+        ReadData readData = new ReadData("input.txt");
+        ProblemSolver problemSolver = new ProblemSolver(readData.getOutput());
+        assertEquals(11744693538946L, problemSolver.calculateGrandTotalForPart2());
+    }
 }

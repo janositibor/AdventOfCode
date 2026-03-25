bb9759bd6a8db68bd630b284ff2a6564ec6922e8
janositibor <janositibor@gmail.com>
2026-03-25 15:59:45 +0100

    2018 day09 part1 solved

---
+++ src/test/java/tzjanosi/y2018/day09/MarbleGame.java
@@ -0,0 +1,74 @@
+package tzjanosi.y2018.day09;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MarbleGame {
+    private static final int STAR = 23;
+    private int numberOfPlayers;
+    private int lastMarble;
+    private List<Integer> marbles = new LinkedList<>(List.of(0));
+    List<Integer> results = new ArrayList<>();
+
+    public MarbleGame(int numberOfPlayers, int lastMarble) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.lastMarble = lastMarble;
+        buildResults();
+        buildGame();
+    }
+
+    private void buildResults() {
+        for (int i = 0; i < numberOfPlayers; i++) {
+            results.add(0);
+        }
+    }
+
+    public int findWinnerScore() {
+        return results.stream().max(Comparator.naturalOrder()).orElseThrow(() -> new IllegalStateException("Empty result list!"));
+    }
+
+    private void buildGame() {
+        int index = 0;
+        for (int i = 1; i <= lastMarble; i++) {
+            if (i % STAR != 0) {
+                index = addMarble(index, i);
+            } else {
+                index = starAction(index, i);
+            }
+        }
+    }
+
+    private int starAction(int index, int value) {
+        int actualPlayer = value % numberOfPlayers;
+        int result = results.get(actualPlayer);
+        int actualIndex = getActualIndexForStarAction(index);
+        int gained = value + marbles.remove(actualIndex);
+        results.set(actualPlayer, result + gained);
+        return actualIndex;
+    }
+
+    private int getActualIndexForStarAction(int index) {
+        int result = index - 7;
+        if (result > 0) {
+            return result;
+        }
+        return (result + marbles.size());
+    }
+
+    private int addMarble(int index, int value) {
+        int actualIndex = getActualIndexForAdd(index);
+        marbles.add(actualIndex, value);
+        return actualIndex;
+    }
+
+    private int getActualIndexForAdd(int index) {
+        if (index + 2 > marbles.size()) {
+            return 1;
+        } else {
+            return (index + 2);
+        }
+    }
+
+}
---
+++ src/test/java/tzjanosi/y2018/day09/MarbleGameTest.java
@@ -0,0 +1,50 @@
+package tzjanosi.y2018.day09;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarbleGameTest {
+    @Test
+    void findWinnerScoreTest() {
+        MarbleGame marbleGame = new MarbleGame(9, 25);
+        assertEquals(32, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreTest2() {
+        MarbleGame marbleGame = new MarbleGame(10, 1618);
+        assertEquals(8317, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreTest3() {
+        MarbleGame marbleGame = new MarbleGame(13, 7999);
+        assertEquals(146373, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreTest4() {
+        MarbleGame marbleGame = new MarbleGame(17, 1104);
+        assertEquals(2764, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreTest5() {
+        MarbleGame marbleGame = new MarbleGame(21, 6111);
+        assertEquals(54718, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreTest6() {
+        MarbleGame marbleGame = new MarbleGame(30, 5807);
+        assertEquals(37305, marbleGame.findWinnerScore());
+    }
+
+    @Test
+    void findWinnerScoreProblemDataTest() {
+        MarbleGame marbleGame = new MarbleGame(458, 71307);
+        assertEquals(398048, marbleGame.findWinnerScore());
+    }
+
+}

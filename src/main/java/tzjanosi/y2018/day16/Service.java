bd1266f846a0d94e6afdc647612603df8856f020
janositibor <janositibor@gmail.com>
2026-05-04 15:27:16 +0200

    2018 day16 part2 solved

--- src/main/java/tzjanosi/y2018/day16/Service.java
+++ src/main/java/tzjanosi/y2018/day16/Service.java
@@ -2,12 +2,37 @@ package tzjanosi.y2018.day16;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Service {
     private List<List<List<Integer>>> input = new ArrayList<>();
+    private Map<Integer, List<Operator>> possibleOperators = new ConcurrentHashMap<>();
+    private List<List<Integer>> inputProgram = new ArrayList<>();
 
-    public Service(List<String> input) {
+    public Service(List<String> input, List<String> inputProgram) {
         processInput(input);
+        processInputProgram(inputProgram);
+    }
+
+    public int execute() {
+        deCrypt();
+        List<Integer> registers = List.of(0, 0, 0, 0);
+        for (int i = 0; i < inputProgram.size(); i++) {
+            List<Integer> arguments = inputProgram.get(i);
+            Operator operator = possibleOperators.get(arguments.get(0)).get(0);
+            Calculator calculator = new Calculator(registers);
+            registers = calculator.calculate(operator, arguments);
+        }
+        return registers.get(0);
+    }
+
+    private void processInputProgram(List<String> inputProgram) {
+        for (int i = 0; i < inputProgram.size(); i++) {
+            String line = inputProgram.get(i);
+            List<Integer> arguments = createListFrom(line, " ");
+            this.inputProgram.add(arguments);
+        }
     }
 
     public int numberOfValidRules() {
@@ -20,8 +45,50 @@ public class Service {
         return count;
     }
 
-    private int numberOfPossibleOpCodes(List<List<Integer>> input) {
-        int count = 0;
+    private void deCrypt() {
+        for (int i = 0; i < input.size(); i++) {
+            int numberOfOperator = input.get(i).get(1).get(0);
+            List<Operator> possibleOperators = possibleOpcodes(input.get(i));
+            process(numberOfOperator, possibleOperators);
+        }
+    }
+
+    private void process(int numberOfOperator, List<Operator> operators) {
+        if (possibleOperators.keySet().contains(numberOfOperator)) {
+            List<Operator> newOperators = confine(numberOfOperator, operators);
+            if (newOperators.size() == 1) {
+                removeOperatorFromPossible(newOperators.get(0));
+            }
+            possibleOperators.put(numberOfOperator, newOperators);
+        } else {
+            possibleOperators.put(numberOfOperator, operators);
+        }
+    }
+
+    private List<Operator> confine(int numberOfOperator, List<Operator> operators) {
+        List<Operator> output = new ArrayList<>(possibleOperators.get(numberOfOperator));
+        List<Operator> operatorsToRemove = new ArrayList<>();
+        for (Operator operatorToCheck : output) {
+            if (!operators.contains(operatorToCheck)) {
+                operatorsToRemove.add(operatorToCheck);
+            }
+        }
+        output.removeAll(operatorsToRemove);
+        return output;
+    }
+
+    private void removeOperatorFromPossible(Operator operator) {
+        for (Map.Entry<Integer, List<Operator>> entry : possibleOperators.entrySet()) {
+            List<Operator> validOperators = entry.getValue();
+            if (validOperators.contains(operator)) {
+                validOperators.remove(operator);
+                possibleOperators.put(entry.getKey(), validOperators);
+            }
+        }
+    }
+
+    private List<Operator> possibleOpcodes(List<List<Integer>> input) {
+        List<Operator> output = new ArrayList<>();
         List<Integer> inputRegister = input.get(0);
         List<Integer> arguments = input.get(1);
         List<Integer> outputRegister = input.get(2);
@@ -29,10 +96,15 @@ public class Service {
         Operator[] operators = Operator.values();
         for (Operator operator : operators) {
             if (outputRegister.equals(calculator.calculate(operator, arguments))) {
-                count++;
+                output.add(operator);
             }
         }
-        return count;
+        return output;
+    }
+
+    private int numberOfPossibleOpCodes(List<List<Integer>> input) {
+        return possibleOpcodes(input).size();
+
     }
 
     private void processInput(List<String> input) {
--- src/test/java/tzjanosi/y2018/day16/ServiceTest.java
+++ src/test/java/tzjanosi/y2018/day16/ServiceTest.java
@@ -8,15 +8,25 @@ class ServiceTest {
     @Test
     void numberOfValidRulesTest() {
         ReadData readData = new ReadData("testInput.txt");
-        Service service = new Service(readData.getOutput());
+        ReadData readDataP2 = new ReadData("input_p2.txt");
+        Service service = new Service(readData.getOutput(), readDataP2.getOutput());
         assertEquals(1, service.numberOfValidRules());
     }
 
     @Test
     void numberOfValidRulesProblemDataTest() {
         ReadData readData = new ReadData("input_p1.txt");
-        Service service = new Service(readData.getOutput());
+        ReadData readDataP2 = new ReadData("input_p2.txt");
+        Service service = new Service(readData.getOutput(), readDataP2.getOutput());
         assertEquals(636, service.numberOfValidRules());
+    }
+
+    @Test
+    void deCryptProblemDataTest() {
+        ReadData readData = new ReadData("input_p1.txt");
+        ReadData readDataP2 = new ReadData("input_p2.txt");
+        Service service = new Service(readData.getOutput(), readDataP2.getOutput());
+        assertEquals(674, service.execute());
     }
 
 }
